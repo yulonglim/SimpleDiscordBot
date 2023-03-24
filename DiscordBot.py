bf6f7b0e7921e3ceeeabb97863afda212c03fa98
yulonglim <yulonglimbleh@gmail.com>
2023-03-24 23:46:44 +0800

    Add Queue functions

--- DiscordBot.py
+++ DiscordBot.py
@@ -6,7 +6,7 @@ import yt_dlp as youtube_dl
 from youtube_search import YoutubeSearch
 import asyncio
 from discord import FFmpegPCMAudio
-
+import collections 
 
 load_dotenv()
 TOKEN = os.getenv("DISCORDBOT")
@@ -17,7 +17,6 @@ intents.presences = False
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
 
 ffmpeg_options = {
     "options": "-vn",
@@ -44,6 +43,11 @@ ytdl_format_options = {
         }
     ],
 }
+
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+Song = collections.namedtuple("Song", "source title requester")
+song_queue = collections.deque()
 
 
 class YTDLSource(discord.PCMVolumeTransformer):
@@ -85,8 +89,19 @@ async def ensure_voice(ctx):
             await ctx.author.voice.channel.connect()
         else:
             raise InvalidVoiceChannel("You are not connected to a voice channel.")
-    elif ctx.voice_client.is_playing():
-        ctx.voice_client.stop()
+
+async def play_next_song(ctx):
+    if song_queue:
+        song = song_queue.popleft()
+        ctx.voice_client.play(song.source, after=lambda _: bot.loop.call_soon_threadsafe(play_next_song_if_not_playing, ctx))
+        await ctx.send(f"Now playing: {song.title}, requested by {song.requester}")
+    else:
+        await ctx.voice_client.disconnect()
+
+
+def play_next_song_if_not_playing(ctx):
+    if not ctx.voice_client.is_playing() and not ctx.voice_client.is_paused():
+        bot.loop.create_task(play_next_song(ctx))
 
 
 @bot.command(name="play", help="Play a song from YouTube.")
@@ -102,10 +117,14 @@ async def play(ctx, *, search: str):
                 url = search
 
             player = await YTDLSource.from_url(url, loop=bot.loop, stream=True)
-            ctx.voice_client.play(
-                player, after=lambda e: print(f"Player error: {e}") if e else None
-            )
-            await ctx.send(f"Now playing: {player.title}")
+            song = Song(source=player, title=player.title, requester=ctx.author.name)
+            song_queue.append(song)
+
+            if not ctx.voice_client.is_playing() and not ctx.voice_client.is_paused():
+                await play_next_song(ctx)
+            else:
+                await ctx.send(f"Added to queue: {song.title}, requested by {song.requester}")
+
     except Exception as e:
         await ctx.send(f"Error occurred: {str(e)}")
 
@@ -113,14 +132,35 @@ async def play(ctx, *, search: str):
 @bot.command(name="stop", help="Stop playing and disconnect from the voice channel.")
 async def stop(ctx):
     if ctx.voice_client:
+        song_queue.clear()
+        ctx.voice_client.stop()
         await ctx.voice_client.disconnect()
+        await ctx.send("Stopped playing and cleared the queue.")
     else:
         await ctx.send("I am not currently connected to a voice channel.")
+
+
+@bot.command(name="skip", help="Skip the current song and play the next one in the queue.")
+async def skip(ctx):
+    if ctx.voice_client.is_playing():
+        ctx.voice_client.stop()
+        await ctx.send("Skipped the current song.")
+    else:
+        await ctx.send("There is no song currently playing.")
+
+@bot.command(name="queue", help="Display the current queue of songs.")
+async def display_queue(ctx):
+    if song_queue:
+        queue_text = "Current queue:\n"
+        for i, song in enumerate(song_queue, start=1):
+            queue_text += f"{i}. {song.title} (requested by {song.requester})\n"
+        await ctx.send(queue_text)
+    else:
+        await ctx.send("The song queue is empty.")
 
 
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} has connected to Discord!")
-
 
 bot.run(TOKEN)
